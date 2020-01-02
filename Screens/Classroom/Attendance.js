4ac9ac21c84e99fd5561305081e1019a378ad9d0
Piyush <ppdpiyush@gmail.com>
2020-01-02 11:35:31 +0530

    attendance page done

--- Screens/Classroom/Attendance.js
+++ Screens/Classroom/Attendance.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,TouchableOpacity } from 'react-native';
 
 class Attendance extends Component {
     static navigationOptions = {
@@ -24,13 +24,33 @@ class Attendance extends Component {
   constructor(props) {
     super(props);
     this.state = {
+        color1:'white',
+        color2:'white'
     };
   }
 
   render() {
     return (
-      <View>
-        <Text> Attendance </Text>
+      <View style={{alignContent:'center',alignItems:'center',justifyContent: 'space-evenly',}}>
+        <Text style={{fontWeight:'bold',marginTop:20}}> Attendance </Text>
+        <Text style={{fontWeight:'normal',marginTop:20}}> Check Those Who were present </Text>
+        <TouchableOpacity onPress={()=>this.setState({color1:this.state.color1==='white'?'#6EF31A':'white'})} style={{height:29,width:265,backgroundColor:this.state.color1,shadowColor: "#000",alignContent:'flex-start',justifyContent:'center',alignItems:'flex-start',marginTop:15,shadowOffset: {width: 0,height: 3,},shadowOpacity: 0.27,shadowRadius: 4.65,elevation: 6,}}>
+            <Text style={{color:this.state.color1==='white'?'black':'white',marginLeft:20}}>
+                1. Honey Singh
+            </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>this.setState({color2:this.state.color2==='white'?'#6EF31A':'white'})} style={{height:29,width:265,backgroundColor:this.state.color2,shadowColor: "#000",alignContent:'flex-start',justifyContent:'center',alignItems:'flex-start',marginTop:15,shadowOffset: {width: 0,height: 3,},shadowOpacity: 0.27,shadowRadius: 4.65,elevation: 6,}}>
+            <Text style={{color:this.state.color2==='white'?'black':'white',marginLeft:20}}>
+                2. Raftaar
+            </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={()=>console.log("next Tapped")} style={{height:29,width:265,backgroundColor:'#6EF31A',shadowColor: "#000",alignContent:'flex-start',justifyContent:'center',alignItems:'flex-start',marginTop:15,shadowOffset: {width: 0,height: 3,},shadowOpacity: 0.27,shadowRadius: 4.65,elevation: 6,}}>
+            <Text style={{color:'white',alignSelf:'center'}}>
+                Next
+            </Text>
+        </TouchableOpacity>
+        
       </View>
     );
   }
